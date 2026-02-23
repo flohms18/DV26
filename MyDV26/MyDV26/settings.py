56de718ec5120f6cdb81347772d994c94f5f501a
flohms18 <trouilletflorent@outlook.com>
2026-02-23 12:00:58 +0100

    Added Bootstrap

--- MyDV26/MyDV26/settings.py
+++ MyDV26/MyDV26/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'WDV26'
+    'WDV26',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
